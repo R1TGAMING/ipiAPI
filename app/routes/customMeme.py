32c716d71742eeaff2f9e4b4c89aa506e3e3a85e
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-07-06 08:55:49 +0000

    Custome Meme Done

--- app/routes/customMeme.py
+++ app/routes/customMeme.py
@@ -3,19 +3,25 @@ from PIL import Image, ImageDraw, ImageFont
 from app.routes import requests, base64, io
 from flask import request, send_file
 
-@app.get("/customMemes")
+@app.get("/customMeme")
 def customMemes() :
   imageQuery = str(request.args.get("img"))
-  #posXQuery = request.args.get("posX")
-  #posYQuery = request.args.get("posY")
-  #textQuery = request.args.get("text")
+  posXQuery = int(request.args.get("posX")) 
+  posYQuery = int(request.args.get("posY")) 
+  textQuery = str(request.args.get("text"))
 
   req = requests.get(imageQuery)
   try :
     image = Image.open(io.BytesIO(req.content))
     dst = Image.new("RGB", (image.width, image.height))
     dst.paste(image)
-    ImageDraw.Draw(dst)
+
+    
+    draw = ImageDraw.Draw(dst)
+    font = ImageFont.truetype("app/static/fonts/ObelixProB-cyr.ttf", 80)
+    W, H = image.size
+    draw.text((W / 2 + posXQuery, H / 2 + posYQuery), text=textQuery, fill=(255, 255, 255), font=font, stroke_fill = (0,0,0), stroke_width=2)
+    
     bytes = io.BytesIO()
     save = dst.save(bytes, format="JPEG")
     bytes.seek(0)
